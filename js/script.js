bb9219fc2e5fc5d473f7e132d295f18bc15dccb8
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-06 17:55:24 +0500

    slide final

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@ menuIcon.addEventListener("click", () => {
 menuKres.addEventListener("click", () => {
     menuModal.classList.remove('transfor');
 })
+
+// Slider
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
